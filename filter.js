f6c7a7dcaf6dba051d4c6264f47ebae740f8c444
prockson47 <119786253+prockson47@users.noreply.github.com>
2023-03-09 15:14:08 +0000

    done finally

--- filter.js
+++ filter.js
@@ -5,3 +5,11 @@ const filterModal = document.getElementById("filter-modal");
 filterIcon.addEventListener("click", function () {
   filterModal.style.display = "block";
 });
+
+
+// Add event listener to the search button to close the filter modal
+const closeBtn = document.getElementById("pop-search");
+closeBtn.addEventListener("click", function () {
+  filterModal.style.display = "none";
+});
+
